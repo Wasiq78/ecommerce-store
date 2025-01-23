0fc55589cb1ce170c369e5821468ffef05842377
wasiq.dev <alikhanwasiq@gmail.com>
2025-01-24 01:33:22 +0500

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+<h1 className=" text-3xl bg-blue-500 font-bold container">App Page</h1>
+<p className="text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi asperiores eos aspernatur debitis? Sit, rerum, libero reprehenderit numquam tempora dolor, eligendi a ullam expedita tempore vel.</p>
+    </>
+  );
+}
+
+export default App;
