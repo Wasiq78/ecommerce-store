6f7520e8d061b4f8e64a1e11e1835e0bd0e1b2d8
wasiq.dev <alikhanwasiq@gmail.com>
2025-02-05 00:54:50 +0500

    Refactored Card COmponent

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 import banner from "../img/banner/banner1.jpg";
 import { useNavigate } from "react-router-dom";
 import { motion, useInView } from "framer-motion";
-import { scrollVariant, fadeLeft, fadeRight } from "../Variants/Variants";
+import { scrollVariant} from "../Variants/Variants";
 
 function Card() {
   const navigate = useNavigate();
@@ -37,10 +37,10 @@ function Card() {
     >
       <div className="container">
         <div className="flex w-full">
-          <motion.div variants={fadeLeft}  className="hidden sm:block w-full">
+          <div className="hidden sm:block w-full">
             <img src={banner} className="h-[24rem]" alt="Banner" />
-          </motion.div>
-          <motion.div variants={fadeRight}  className="flex flex-col justify-center bg-gray-300 w-full">
+          </div>
+          <div  className="flex flex-col justify-center bg-gray-300 w-full">
             <div className="px-8 py-16 sm:py-0 md:px-12 lg:px-20">
               <h3 className="text-2xl font-bold">Creative harmonious living</h3>
               <p className="text-xl my-4">
@@ -55,7 +55,7 @@ function Card() {
                 Shop Now
               </button>
             </div>
-          </motion.div>
+          </div>
         </div>
       </div>
     </motion.div>
